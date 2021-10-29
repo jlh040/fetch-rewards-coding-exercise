bf98251d4ee0665cd517823393718749b59dbfc3
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-28 21:14:19 -0700

    created a 404 error handler

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  const notFoundError = new ExpressError('Not found', 404);
+  return next(notFoundError);
+})
+
 app.use((err, req, res, next) => {
   let status = err.status || 500;
   let message = err.message;
