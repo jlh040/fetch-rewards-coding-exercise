2cfcef7999d32dc7f5cd19cf4edb976572536c0f
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-28 21:40:16 -0700

    set up some variables to hold the app data

--- routes.js
+++ routes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = new express.Router();
 
+// fake partners table
 const partners = [
   {payer: 'DANNON', points: 0},
   {payer: 'UNILEVER', points: 0},
   {payer: 'MILLER COORS', points: 0}
 ];
+
+// fake transactions table
+const transactions = [];
+
 
 router.get('/points', (req, res, next) => {
   return res.json(partners);
