ef2856b51a017ef569742231f30fc61f71badaa9
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-28 21:26:27 -0700

    request/response cycle is functional

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const routes = require('./routes');
 const ExpressError = require('./expressError');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(routes);
 app.use(express.json());
 
 app.use((req, res, next) => {
--- routes.js
+++ routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const app = require('./server');
 const router = new express.Router();
 
-app.get('/dog', (req, res, next) => {
+router.get('/dog', (req, res, next) => {
   return res.json({
     'name': 'WOOFY'
   });
