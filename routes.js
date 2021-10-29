2a6d958acc1edfcdb701a67c1d92d9079c8e6944
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-28 21:31:17 -0700

    made a variable which holds all current partners

--- routes.js
+++ routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = new express.Router();
 
-router.get('/dog', (req, res, next) => {
+const payers = [
+  {payer: 'DANNON', points: 0},
+  {payer: 'UNILEVER', points: 0},
+  {payer: 'MILLER COORS', points: 0}
+];
+
+router.get('/points', (req, res, next) => {
   return res.json({
-    'name': 'WOOFY'
+    payers
   });
 })
 
